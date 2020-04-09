d6158fd65408f676639f3cbc01a263a9d33b421c
Carmine Calicchio <calicchiocarmine@gmail.com>
2020-04-09 23:36:45 +0100

    adding users controller

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,6 +1,7 @@
 class Coin < ApplicationRecord
 
   @@base_uri = 'https://api.coincap.io/v2/assets/'
+  
   def self.new_lookup(coin_name)
     uri = URI(@@base_uri + coin_name)
     JSON.parse(Net::HTTP.get(uri))["data"]
