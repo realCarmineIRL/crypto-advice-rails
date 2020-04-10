247531466c9cca7f96207186c7f6bacda72de828
Carmine Calicchio <calicchiocarmine@gmail.com>
2020-04-10 20:24:46 +0100

    downcasing coin name

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -7,7 +7,8 @@ class Coin < ApplicationRecord
 
   @@base_uri = 'https://api.coincap.io/v2/assets/'
 
-  def self.new_lookup(coin_name)
+  def self.new_lookup(name)
+    coin_name = name.downcase
     uri = URI(@@base_uri + coin_name)
     res = JSON.parse(Net::HTTP.get(uri))["data"]
 
