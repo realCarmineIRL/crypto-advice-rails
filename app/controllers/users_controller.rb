af7187d8759a379297cfe9cb3df5d43092309faf
Carmine Calicchio <calicchiocarmine@gmail.com>
2020-04-11 02:10:01 +0100

    update last_price

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def my_portfolio
+    current_user.coins.each do |coin|
+      Coin.update_price(coin.name)
+    end
     @tracked_coins = current_user.coins
   end
 end
